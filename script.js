5e2661595a48ade04e7de140eb46bedd6ba8c39c
Jarno Beyers <jarno.beyers@student.ap.be>
2026-09-25 11:20:40 +0200

    Alle json files zijn compleet. De RAW github image links staan correct in de JSON files. De index.html pagina toont een overzicht ter controle als de RAW github links werken en op de juiste plaats staan per planeet/galaxy, deze worden gegenereerd via JavaScript in script.js.

--- script.js
+++ script.js
@@ -1,40 +1,40 @@
-import planets from "./planets.json" with {type: "json"};
-import galaxies from "./galaxies.json" with {type: "json"};
+import planets from "./planets.json" with { type: "json" };
+import galaxies from "./galaxies.json" with { type: "json" };
 
 const planetList = document.querySelector("#planet-list");
 const planetGalaxyList = document.querySelector("#planet-galaxy-list");
 const galaxyList = document.querySelector("#galaxy-list");
 
-planets.forEach(planet => {
-    const li = document.createElement("li");
-    const a = document.createElement("a");
+planets.forEach((planet) => {
+  const li = document.createElement("li");
+  const a = document.createElement("a");
 
-    a.href = planet.imageUrl;
-    a.target = "_blank";
-    a.textContent = planet.name;
+  a.href = planet.imageUrl;
+  a.target = "_blank";
+  a.textContent = planet.name;
 
-    li.appendChild(a);
-    planetList.appendChild(li);
-// ---------------------------------------------
-    const liGalaxy = document.createElement("li");
-    const aGalaxy = document.createElement("a");
+  li.appendChild(a);
+  planetList.appendChild(li);
+  // ---------------------------------------------
+  const liGalaxy = document.createElement("li");
+  const aGalaxy = document.createElement("a");
 
-    aGalaxy.href = planet.galaxy.imageUrl;
-    aGalaxy.target = "_blank";
-    aGalaxy.textContent = planet.galaxy.name;
+  aGalaxy.href = planet.galaxy.imageUrl;
+  aGalaxy.target = "_blank";
+  aGalaxy.textContent = planet.galaxy.name;
 
-    liGalaxy.appendChild(aGalaxy);
-    planetGalaxyList.appendChild(liGalaxy);
+  liGalaxy.appendChild(aGalaxy);
+  planetGalaxyList.appendChild(liGalaxy);
 });
 
-galaxies.forEach(galaxy => {
-    const li = document.createElement("li");
-    const a = document.createElement("a");
+galaxies.forEach((galaxy) => {
+  const li = document.createElement("li");
+  const a = document.createElement("a");
 
-    a.href = galaxy.imageUrl;
-    a.target = "_blank";
-    a.textContent = galaxy.name
+  a.href = galaxy.imageUrl;
+  a.target = "_blank";
+  a.textContent = galaxy.name;
 
-    li.appendChild(a);
-    galaxyList.appendChild(li);
-})
+  li.appendChild(a);
+  galaxyList.appendChild(li);
+});
